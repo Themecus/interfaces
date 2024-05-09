919846a0976f8196857b11034143892c8b77bc89
Themecus <miguelangelbros2015@gmail.com>
2024-05-08 21:08:47 -0400

    aqui2

--- src/Carro.java
+++ src/Carro.java
@@ -2,7 +2,7 @@ public class Carro implements Motor {
     @Override
     public void correr() {
         System.out.println("Aqui tenemos gasolina para el diesel");
-        // numero=numero+1; aqui las variables de interfaces deben ser final, static
+        // numero=numero+1; aqui las variables de interfaces deben ser final, staticaaaa
     }
 
     @Override
